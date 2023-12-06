4859708231f2989975b9e9eca58be06447a7b545
lestofante <lesto@fante>
2023-12-06 01:53:10 +0100

    using macro instead of manual code generation

--- day5/module/src/lib.rs
+++ day5/module/src/lib.rs
@@ -1,43 +1,52 @@
 extern crate proc_macro;
 
+use std::{fs, env, path::PathBuf};
+
 use proc_macro::TokenStream;
+use proc_macro2::Ident;
 use quote::quote;
+use syn::parse_macro_input;
 
 #[proc_macro]
-pub fn generate_map(_input: TokenStream) -> TokenStream {
+pub fn generate_map(input: TokenStream) -> TokenStream {
+    let binding = parse_macro_input!(input as syn::LitStr).value();
 
-    let input_string = include_str!("../../data/input.txt").split("\n\n");
+    let srcdir = PathBuf::from(file!());
+    let srcdir = srcdir.parent().unwrap();
+    let binding = srcdir.join(binding);
+
+    let binding = fs::read_to_string(&binding).expect(&format!("file erro {binding:?} {}", file!()));
+
+    let input_string = binding.split("\n\n");
     
-    let mut code = quote! {};
+    let mut all_code = quote! {};
     for parts in input_string.skip(1){
 
         let mut lines = parts.lines();
 
-        let name = lines.next().unwrap().replace(" ", "_").replace(":", "").replace("-", "_");
+        let name = Ident::new(&lines.next().unwrap().trim().replace(" ", "_").replace(":", "").replace("-", "_"), proc_macro2::Span::call_site());
 
         let branches = lines.map(|line| {
             let values: Vec<usize> = line.split_whitespace().map(|s| s.parse::<usize>().unwrap()).collect();
             let dest_start = values[0];
             let src_start = values[1];
-            let src_end = values[1] + values[2];
+            let src_end = values[1] + values[2] - 1;
             quote!{
                 #src_start..=#src_end => #dest_start + i - #src_start,
             }
         });
 
-        
-
-        code = quote! {
-
+        let code = quote!{
             fn #name(i: usize) -> usize {
-                /*match i {
+                match i {
                     #(#branches)*
                     _ => return i,
-                }*/
-                i
+                }
             }
         };
+
+        all_code.extend(code);
     }
 
-    code.into()
+    all_code.into()
 }
